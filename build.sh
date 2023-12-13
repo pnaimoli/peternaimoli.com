4f19de49379af5b1d7028751453fd59182063a72
Peter Naimoli <pnaimoli@users.noreply.github.com>
2023-12-13 13:38:12 -0600

    Update build.sh script.

--- build.sh
+++ build.sh
@@ -2,25 +2,31 @@
 
 set -e
 
+# Set up the "deploy" branch in the build directory if it's not already there.
+if [ ! -d build ]; then
+    echo "Setting up 'build' directory..."
+    git remote show gandi || git remote add gandi git+ssh://9361771@git.sd5.gpaas.net/default.git
+    mkdir build
+    git worktree add --detach build
+    cd build
+    git checkout --orphan deploy
+fi
+
+# Build our submodules
 cd measure-hider
 ./build.sh
 cd ..
 
-mkdir -p build
+rm -rf build/*
+cp -p wsgi.py build/
+cp -p measure-hider/build/requirements.txt build/
+mkdir -p build/measure-hider
+cp -rp measure-hider/build build/measure-hider/
 
-git remote show gandi || git remote add gandi git+ssh://9361771@git.sd5.gpaas.net/default.git
-git worktree add build deploy
-
-#cd client && npm run build; cd ..
-#mkdir -p build/client
-#rm -rf build/client/*
-#cp -r client/build/* build/client/
-#cp -p server/{measure_hider_modeler.py,model.pt,requirements.txt,wsgi.py} build/
-
-#cd build
-#git add -A .
-#git commit -m "Update deploy branch"
-###git push gandi deploy
-###ssh 9361771@git.sd5.gpaas.net 'deploy default.git deploy'
-#cd ..
-#lftp -e 'mirror -P 10 -R build /vhosts/default' -u '9361771,' sftp://sftp.sd5.gpaas.net
+cd build
+git add -A .
+git diff --quiet && git diff --staged --quiet || git commit -m "Update deploy branch"
+#git push gandi deploy
+#ssh 9361771@git.sd5.gpaas.net 'deploy default.git deploy'
+cd ..
+lftp -e 'mirror -P 10 -R build /vhosts/default' -u '9361771,' sftp://sftp.sd5.gpaas.net
